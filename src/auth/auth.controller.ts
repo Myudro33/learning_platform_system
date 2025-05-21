5084c3c905a8d1b73a9f49ac941939b1a583a079
NikaQanashvili <qanashvilinika7@gmail.com>
2025-05-21 18:11:56 +0400

    feat: add sign in

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,17 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { CreateAuthDto } from "./dto/create-user.dto";
-import { AuthService } from "./auth.service";
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateAuthDto } from './dto/create-user.dto';
+import { AuthService } from './auth.service';
+import { SingInDto } from './dto/signin-user.dto';
 
 @Controller('auth')
-export class AuthController{
-    constructor(private authService:AuthService){}
-    @Post('signup')
-    signUp(@Body() dto:CreateAuthDto){
-        return this.authService.signUp(dto)
-    }
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('signup')
+  signUp(@Body() dto: CreateAuthDto) {
+    return this.authService.signUp(dto);
+  }
+  @Post('signin')
+  singIn(@Body() dto: SingInDto) {
+    return this.authService.signIn(dto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,20 +6,23 @@ import {
 import { CreateAuthDto } from './dto/create-user.dto';
 import { PrismaService } from '../prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
-import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { SingInDto } from './dto/signin-user.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
   async signUp(data: CreateAuthDto) {
     const hash = await bcrypt.hash(data.password, 10);
-    const hashedPassword = await argon.hash(data.password);
     try {
       const user = await this.prisma.user.create({
         data: {
           ...data,
-          password: hashedPassword,
+          password: hash,
         },
       });
       return user;
@@ -31,5 +34,40 @@ export class AuthService {
         throw new InternalServerErrorException(error);
       }
     }
+  }
+  async signIn(data: SingInDto) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: { email: data.email },
+      });
+      if (!user) {
+        throw new ForbiddenException('user not found');
+      }
+      const pwMatches = await bcrypt.compare(user.password, data.password);
+      if (!pwMatches) {
+        throw new ForbiddenException('incorect password');
+      }
+      return this.signToken(user.id, user.email, user.roleId);
+    } catch (error) {
+      return error;
+    }
+  }
+  async signToken(
+    userId: number,
+    email: string,
+    role: number,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      id: userId,
+      email,
+      role,
+    };
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '1h',
+      secret: process.env.JWT_SECRET,
+    });
+    return {
+      access_token: token,
+    };
   }
 }
